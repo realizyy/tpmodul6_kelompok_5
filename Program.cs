0c9b08d7948d3a1fc881a37285679248f1129873
realizyy <realizy@student.telkomuniversity.ac.id>
2022-04-01 16:30:11 +0800

    Menambahkan class DataMahasiswa_1302204035

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Newtonsoft.Json;
+    
+namespace tpmodul6_kelompok_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DataMahasiswa1302204035 data = new DataMahasiswa1302204035();
+            data.ReadJSON();
+        }
+    }
+} 
